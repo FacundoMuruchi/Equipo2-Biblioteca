bafc954b1352e0cf0d39bb60d4fc707d61095e13
FMuruchi <facundomuruchi01@gmail.com>
2025-10-07 11:37:22 -0300

    base avl

--- src/tdas/arboles/ABB.java
+++ src/tdas/arboles/ABB.java
@@ -13,7 +13,7 @@ public class ABB implements ArbolBinarioTDA{
 
     @Override
     public void inicializarArbol() {
-
+        raiz = null;
     }
 
     @Override
---
+++ src/tdas/arboles/AVL.java
@@ -0,0 +1,38 @@
+package tdas.arboles;
+
+public class AVL implements ArbolBinarioTDA {
+    @Override
+    public void inicializarArbol() {
+
+    }
+
+    @Override
+    public int raiz() {
+        return 0;
+    }
+
+    @Override
+    public ArbolBinarioTDA hijoIzq() {
+        return null;
+    }
+
+    @Override
+    public ArbolBinarioTDA hijoDer() {
+        return null;
+    }
+
+    @Override
+    public boolean arbolVacio() {
+        return false;
+    }
+
+    @Override
+    public void agregarElem(int x) {
+
+    }
+
+    @Override
+    public void eliminarElem(int x) {
+
+    }
+}
